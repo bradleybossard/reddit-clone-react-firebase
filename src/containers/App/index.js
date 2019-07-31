7c6d90bf8213ef83835b7a85cfdaf607c0d4ea62
Bradley Bossard <bradleybossard@gmail.com>
2019-07-30 21:15:14 -0700

    Fully working Hello World example

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,1 +1,12 @@
 import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      Hello World
+      </div>
+    );
+  }
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -7,3 +8,12 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import registerServiceWorker from './utils/registerServiceWorker';
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+// 'root'
